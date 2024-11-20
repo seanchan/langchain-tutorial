8ea8d6e38112ee0a33f19d7f09e3fce43036312f
chensilun <silunchn@gmail.com>
2024-11-20 15:20:26 +0800

    2.chatbot conversation mode

---
+++ 2.chatbot/index.js
@@ -0,0 +1,34 @@
+const { SystemMessage, HumanMessage } = require("@langchain/core/messages");
+const { StringOutputParser } = require("@langchain/core/output_parsers");
+const { ChatPromptTemplate } = require("@langchain/core/prompts");
+const { ChatOpenAI } = require("@langchain/openai");
+const { HttpsProxyAgent } = require("https-proxy-agent");
+const env = require("dotenv").config();
+const dotenv = env.parsed;
+
+const agent = new HttpsProxyAgent("http://127.0.0.1:7890");
+
+const model = new ChatOpenAI(
+  {
+    model: "gpt-4",
+  },
+  {
+    apiKey: dotenv.OPENAI_API_KEY,
+    httpAgent: agent,
+    configuration: {
+      timeout: 5,
+      httpAgent: agent,
+    },
+  }
+);
+const parser = new StringOutputParser();
+const chain = model.pipe(parser);
+chain
+  .invoke([
+    { role: "user", content: "Hi! I'm Bob" },
+    { role: "assistant", content: "Hello Bob! How can I assist you today?" },
+    { role: "user", content: "What's my name?" },
+  ])
+  .then((response) => {
+    console.log(response);
+  });
