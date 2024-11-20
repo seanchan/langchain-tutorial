c14b9c76fabf09d2785bf1f82a2c146da231e0b9
chensilun <silunchn@gmail.com>
2024-11-20 14:59:06 +0800

    add proxy to call.

--- 1.llm_chain/index.js
+++ 1.llm_chain/index.js
@@ -1,11 +1,25 @@
 const { SystemMessage, HumanMessage } = require("@langchain/core/messages");
 const { StringOutputParser } = require("@langchain/core/output_parsers");
 const { ChatOpenAI } = require("@langchain/openai");
+const { HttpsProxyAgent } = require("https-proxy-agent");
+const env = require("dotenv").config();
+const dotenv = env.parsed;
 
-const model = new ChatOpenAI({
-  model: "gpt-4",
-  timeout: 30,
-});
+const agent = new HttpsProxyAgent("http://127.0.0.1:7890");
+
+const model = new ChatOpenAI(
+  {
+    model: "gpt-3.5-turbo",
+  },
+  {
+    apiKey: dotenv.OPENAI_API_KEY,
+    httpAgent: agent,
+    configuration: {
+      timeout: 5,
+      httpAgent: agent,
+    },
+  }
+);
 
 const messages = [
   new SystemMessage("Translate the following from English into Italian"),
